21440f85ef5a649f04ddf3f4c20e1938037097eb
Amr M. Kayid <amrkayid2027@gmail.com>
2018-09-17 13:30:18 +0200

    Simple server

---
+++ server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"net/http"
+	"fmt"
+	"os"
+)
+
+func Start() {
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/go", goHandler)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	http.ListenAndServe("localhost:" + port, router)
+
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Web from Go!")
+}
+
+func goHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "I <3 Go!")
+}
